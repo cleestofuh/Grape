f119a70ec0bcde1a3be1d7d577d206b960793974
Chris Lo <vagrant@precise32.(none)>
2016-03-01 00:37:08 +0000

    SO MANY CHANGES BUT FIXED GRAPH RESIZING PROBLEM

--- public/js/reflection.js
+++ public/js/reflection.js
@@ -5,82 +5,54 @@ $('select').on(
     }
   , "change": function() {
       choice = $(this).val();
-      document.getElementById('selectedCat').innerHTML = choice.toUpperCase() + ":";
 
-      if(choice == "Love") {
+      document.getElementById('selectedCat').innerHTML = choice + ":";
+
+      if(choice == "Time") {
         document.getElementById('selectedCatDesc').innerHTML = "These acts of kindness made you feel very happy!";
       }
-      else if(choice == 'Compassion') {
+      else if(choice == 'Health') {
         document.getElementById('selectedCatDesc').innerHTML = "These acts of kindness made you feel pretty happy!";
       }
-      else if(choice == 'Time') {
+      else if(choice == 'Energy') {
         document.getElementById('selectedCatDesc').innerHTML = "These acts of kindness made you feel fairly happy!";
       }
-      else if(choice == 'Energy') {
+      else if(choice == 'Other') {
         document.getElementById('selectedCatDesc').innerHTML = "These acts of kindness made you feel somewhat happy.";
       }
-      else if(choice == 'Charity') {
+      else if(choice == 'Love') {
         document.getElementById('selectedCatDesc').innerHTML = "These acts of kindness made you feel the happiest! Keep at it!";
       }
-      else if(choice == 'Health') {
+      else if(choice == 'Charity') {
         document.getElementById('selectedCatDesc').innerHTML = "These acts of kindness made you feel the least happy. What can you do to change this?";
       }
 
     }
   });
-      console.log(document.getElementById('selectedCat').innerHTML);
 
-$(function () {
-    $('#reflectionChart').highcharts({
-        chart: {
-            plotBackgroundColor: null,
-            plotBorderWidth: null,
-            plotShadow: false,
-            type: 'pie'
-        },
-        title: {
-            text: 'YOUR HAPPINESS'
-        },
-        tooltip: {
-            pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
-        },
-        plotOptions: {
-            pie: {
-                allowPointSelect: true,
-                cursor: 'pointer',
-                dataLabels: {
-                    enabled: true,
-                    format: '<b>{point.name}</b>: {point.percentage:.1f} %',
-                    style: {
-                        color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
-                    }
-                }
-            }
-        },
-        series: [{
-            name: 'Brands',
-            colorByPoint: true,
-            data: [{
-                name: 'Charity',
-                y: 56.33
-            }, {
-                name: 'Love',
-                y: 24.03,
-                sliced: true,
-                selected: true
-            }, {
-                name: 'Compassion',
-                y: 10.38
-            }, {
-                name: 'Time',
-                y: 4.77
-            }, {
-                name: 'Energy',
-                y: 0.91
-            }, {
-                name: 'Health',
-                y: 0.2
-            }]
-        }]
-    });
-});
+
+google.charts.load("44", {packages:["corechart"]});
+google.charts.setOnLoadCallback(drawChart);
+function drawChart() {
+  var data = google.visualization.arrayToDataTable([
+    ['GRAPES', 'YOUR HAPPINESS'],
+    ['Love', 9],
+    ['Time', 6.5],
+    ['Energy', 5],
+    ['Charity', 2],
+    ['Health', 6],
+    ['Other', 4]
+  ]);
+
+  var options = {
+    title: 'Your Happiness',
+    pieHole: 0.2,
+    backgroundColor: '#E0D2E7',
+    'chartArea': {'width': '80%', 'height': '80%'},
+    'fontName': 'Open Sans Condensed',
+    'fontSize': '16'
+  };
+
+  var chart = new google.visualization.PieChart(document.getElementById('donutchart'));
+  chart.draw(data, options);
+}
