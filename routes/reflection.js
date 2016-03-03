22f1bad3408c2a3ae9eebbe00d6cf0dd2a294554
Chris Lo <vagrant@precise32.(none)>
2016-03-03 02:02:54 +0000

    added google analytics to all pages

--- routes/reflection.js
+++ routes/reflection.js
@@ -4,10 +4,9 @@ exports.view = function(req, res){
 	var random_num = Math.random();
 	console.log(random_num);
 		if (random_num > 0.25) {
-			flag = true;
+
 			res.render('reflection', {yourkinderact:kindersjson["yourkinders"], otherkinderact:kindersjson["otherkinders"]});
 		} else {
-			flag = false;
 			res.redirect('/reflection2');
 		}
 };
